bc29c2265cdaf7fd45addec3312d04d96afb460c
Mohammad Khatibzadeh <mohammad.khatibzadeh.3@gmail.com>
2020-09-10 20:44:25 +0430

    Basic User SignUp is OK. Basic User Edit Data Is OK. All of them work with JWT and generate one at Signup.

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
-
-
-
+from .views import registration, ManageUserView
 
 
 
@@ -14,4 +12,6 @@ urlpatterns = [
     # Your URLs...
     path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', registration, name='register'),
+    path('api/edit/', ManageUserView.as_view(), name='edit'),
 ]
--- customers/views.py
+++ customers/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, generics
+from rest_framework import response, decorators, permissions, status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import UserSerializer
 
-# Create your views here.
+User = get_user_model()
+
+@decorators.api_view(["POST"])
+@decorators.permission_classes([permissions.AllowAny])
+def registration(request):
+    serializer = UserSerializer(data=request.data)
+    if not serializer.is_valid():
+        return response.Response(serializer.errors, status.HTTP_400_BAD_REQUEST)        
+    user = serializer.save()
+    refresh = RefreshToken.for_user(user)
+    res = {
+        "refresh": str(refresh),
+        "access": str(refresh.access_token),
+    }
+    return response.Response(res, status.HTTP_201_CREATED)
+    
+
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    "Manage The Authennticated User"
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        "retrieve And return authenticated User"
+        return self.request.user
